ce9d2141295b7783fdf25439904e8ea47199224a
mab1k <piv0004@yandex.ru>
2025-05-15 13:40:45 +0300

    Example3

--- src/main/java/org/example/Example2.java
+++ src/main/java/org/example/Example2.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import org.hibernate.query.Query;
+import org.example.config.PropertiesSessionFactoryProvider;
+import org.example.entity.Device;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
 public class Example2 {
+    public static void main(String[] args) {
+        PropertiesSessionFactoryProvider propertiesSessionFactoryProvider = new PropertiesSessionFactoryProvider();
+        SessionFactory sessionFactory = propertiesSessionFactoryProvider.getSessionFactory();
+
+        try(Session session = sessionFactory.openSession()){
+            // ВЫБОРКА ВСЕХ ЗНАЧЕНИЙ ИЗ ТАБЛИЦЫ devices
+            Query<Device> query = session.createQuery("from Device where installationDate = current_date()", Device.class);
+            List<Device> resultList = query.getResultList();
+            for (Device el : resultList){
+                System.out.println(el);
+            }
+
+        }
+        catch (HibernateException e){
+            e.printStackTrace();
+        }
+    }
+
 }
