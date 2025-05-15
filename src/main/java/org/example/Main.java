9acf061a74ef829ab522ba93ada6c248afb9512b
mab1k <piv0004@yandex.ru>
2025-05-15 13:21:34 +0300

    query many use

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.config.PropertiesSessionFactoryProvider;
 import org.example.entity.Device;
 import org.hibernate.HibernateException;
+import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -25,9 +26,31 @@ public class Main {
 //            }
 
             // ВЫБОРКА ОДНОЙ ЗАПИСИ
-            Query<Device> query = session.createQuery("from Device where id = 1", Device.class);
-            Device singleResult = query.getSingleResult();
-            System.out.println(singleResult);
+//            Query<Device> query = session.createQuery("from Device where id = 1", Device.class);
+//            Device singleResult = query.getSingleResult();
+//            System.out.println(singleResult);
+
+            // ВЫБОРКА ВСЕХ ЗНАЧЕНИЙ ИЗ ТАБЛИЦЫ devices STREAM API
+//            Query<Device> query = session.createQuery("from Device ", Device.class);
+
+           // query.getResultStream().forEach(System.out::println);
+//            Device device = query.getResultStream().findFirst().orElseThrow();
+//            System.out.println(device);
+            // ВЫБОРКА ВСЕХ ЗНАЧЕНИЙ ИЗ ТАБЛИЦЫ devices SCROLL
+            Query<Device> query = session.createQuery("from Device ", Device.class);
+            ScrollableResults<Device> scroll = query.scroll();
+            scroll.position(3);
+            while (scroll.next()){
+                Device device = scroll.get();
+                System.out.println(device);
+            }
+
+            List<Device> list = query.list();
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+
+
         }
         catch (HibernateException e){
             e.printStackTrace();
